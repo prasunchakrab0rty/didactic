fb8dbfdc22035d9b28599033f845ec0e07df3449
Prasun Chakraborty <lordprasun@gmail.com>
2020-11-18 10:18:41 +0530

    Added Style and Welcome PAge

--- frontEndAngular/src/app/app.module.ts
+++ frontEndAngular/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
